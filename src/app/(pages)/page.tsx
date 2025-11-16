8b3f45c87eb20d0cae23b9539c091ce1769fdff9
vlikaa <aliveliyev123654@gmail.com>
2025-11-16 22:33:26 +0400

    add: extend home page with header & footer, simplify layout

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/layout/header';
+import Footer from '@/components/layout/footer';
+
+export default function Home() {
+	return (
+		<>
+			<Header />
+			<main>
+				Main
+			</main>
+			<Footer />
+		</>
+	);
+}
