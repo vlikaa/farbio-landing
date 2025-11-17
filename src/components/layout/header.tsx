280f2b96d85ef2edaa14705a2c1a0a6c4750083a
vlikaa <aliveliyev123654@gmail.com>
2025-11-17 11:31:55 +0400

    add: extend header with NavigationBar and additional elements

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,10 @@
-"use client"
+'use client'
 
 import Link from 'next/link';
 import Logo from '@/components/ui/logo';
+import NavigationBar from '@/components/ui/navigation-bar';
+import Button from '@/components/ui/button';
+import LanguageSwitcher from '@/components/ui/language-switcher';
 
 const navigation = [
 	{ name: 'Услуги', href: '#services' },
@@ -15,9 +18,15 @@ export default function Header() {
 		<header>
 			<div className="flex items-center justify-between h-[50px] px-[20px] md:px-[60px]">
 				<Link href="/">
-					<Logo />
+					<Logo/>
 				</Link>
 
+				<NavigationBar items={ navigation }/>
+
+				<div className="flex gap-[20px]">
+					<LanguageSwitcher/>
+					<Button title={ 'позвонить' }/>
+				</div>
 			</div>
 		</header>
 	)
