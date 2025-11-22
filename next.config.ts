fca26aeea9b8573b10c6d4979d465f86912b1520
vlikaa <aliveliyev123654@gmail.com>
2025-11-23 01:42:37 +0400

    feat: enhance multi-language support with new translations and components

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
+import {NextConfig} from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	reactCompiler: true,
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
