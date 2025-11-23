1df50f960c2d1a9b3aa5cdbd19a0644b9ac12639
vlikaa <aliveliyev123654@gmail.com>
2025-11-23 21:07:35 +0400

    feat: add ContactItem component for displaying contact information with customizable props and responsive design

---
+++ src/components/ui/contact-item.tsx
@@ -0,0 +1,24 @@
+interface ContactItemProps {
+	icon: React.ReactNode;
+	label: string;
+	value: string;
+	className?: string;
+}
+
+export default function ContactItem({ icon, label, value, className }: ContactItemProps) {
+	return (
+		<div className={ `flex justify-center items-center gap-[12px] md:gap-[20px] ${ className }` }>
+			<div className="flex-shrink-0 w-[30px] h-[30px] md:w-[50px] md:h-[50px]">
+				{ icon }
+			</div>
+			<div className="flex flex-col justify-center gap-[5px]">
+				<span className="text-[16px] md:text-[20px] font-inter font-light md:font-bold text-white">
+					{ label }
+				</span>
+				<span className="text-[8px] md:text-[16px] font-inter font-extralight leading-[10px] text-white">
+					{ value }
+				</span>
+			</div>
+		</div>
+	);
+}
