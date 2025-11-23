0c81b9c407b27656e6f490293a37dfaac589c4f8
vlikaa <aliveliyev123654@gmail.com>
2025-11-23 21:04:40 +0400

    feat: enhance Footer component with internationalization support, improved layout, and social media links for better user engagement

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,107 @@
+'use client'
+
+import { useTranslations } from 'use-intl';
+import Logo from '@/components/ui/logo';
+import { FacebookIcon } from '@/components/icons/facebook-icon';
+import { WhatsappIcon } from '@/components/icons/whatsapp-icon';
+import { TelegramIcon } from '@/components/icons/telegram-icon';
+import { InstagramIcon } from '@/components/icons/instagram-icon';
+import SocialIcon from '@/components/ui/social-icon';
+
 export default function Footer() {
+	const t = useTranslations('Footer');
+	const consultationForm = useTranslations('ConsultationForm');
+
 	return (
-		<footer>
-			Footer
+		<footer className="xl:flex xl:flex-col xl:items-center py-[50px] md:py-[100px] px-[20px] md:px-[60px] bg-[#160D32]">
+			<div className="grid gap-x-[50px] md:gap-x-0">
+				<div className="flex flex-col justify-center items-center col-span-2 md:col-span-1 md:justify-start md:items-start">
+					<Logo width={ 52 } height={ 50 }/>
+
+					<p className="hidden md:block w-[300px] xl:w-[372px] mt-[20px] mb-[27px] xl:mb-[45px] text-[12px] font-inter font-extralight text-white">
+						{ t('description') }
+					</p>
+
+					<p className="hidden md:block text-[12px] font-inter font-extralight text-white">
+						{ t('copyright') }
+					</p>
+				</div>
+
+				<p className="md:hidden col-span-2 md:col-span-1 md:row-start-2 mt-[20px] mb-[40px] text-[8px] md:text-[12px] font-inter font-extralight text-center md:text-left text-white">
+					{ t('description') }
+				</p>
+
+				<div className="md:col-start-2 md:row-start-1 md:mb-[34px] xl:mb-0 xl:ml-[21px] xl:mr-[280px] 2xl:ml-[150px] 2xl:mr-[380px]">
+					<h3 className="mb-[20px] md:mb-[30px] xl:mb-[46px] text-[16px] md:text-[20px] font-inter font-light md:font-bold text-white">
+						{ t('services') }
+					</h3>
+					<ul className="flex flex-col gap-[10px]">
+						<li className="text-[12px] font-inter font-extralight text-white">
+							{ t('servicesList.lawn') }
+						</li>
+						<li className="text-[12px] font-inter font-extralight text-white">
+							{ t('servicesList.planting') }
+						</li>
+						<li className="text-[12px] font-inter font-extralight text-white">
+							{ t('servicesList.irrigation') }
+						</li>
+						<li className="text-[12px] font-inter font-extralight text-white">
+							{ t('servicesList.pruning') }
+						</li>
+					</ul>
+				</div>
+
+				<div className="md:col-start-2 md:row-start-2  xl:col-start-3 xl:row-start-1">
+					<h3 className="mb-[20px] md:mb-[30px] xl:mb-[46px] text-[16px] md:text-[20px] font-inter font-light md:font-bold text-white">
+						{ t('contacts') }
+					</h3>
+					<ul className="flex flex-col gap-[10px]">
+						<li className="text-[12px] font-inter font-extralight text-white">
+							{ consultationForm('phoneNumber') }
+						</li>
+						<li className="text-[12px] font-inter font-extralight text-white">
+							{ consultationForm('emailAddress') }
+						</li>
+					</ul>
+				</div>
+
+				<div className="col-span-2 md:col-span-1 md:col-start-2 xl:col-start-3 flex items-center justify-center md:justify-start gap-[20px] my-[50px] xl:mt-0">
+					<SocialIcon
+						icon={ <WhatsappIcon/> }
+						href="https://wa.me/994506748535"
+					/>
+					<SocialIcon
+						icon={ <FacebookIcon/> }
+						href="https://facebook.com"
+					/>
+					<SocialIcon
+						icon={ <TelegramIcon/> }
+						href="https://t.me"
+					/>
+					<SocialIcon
+						icon={ <InstagramIcon/> }
+						href="https://instagram.com"
+					/>
+				</div>
+
+				<p className="md:hidden col-span-2 md:col-span-1 md:row-start-3 md:col-start-1 text-[12px] font-inter font-extralight text-white text-center">
+					{ t('copyright') }
+				</p>
+			</div>
+
+			<div className="max-w-3xl 2xl:max-w-5xl w-full h-px bg-white/30 mt-[40px] mb-[50px]"/>
+
+			<div className="flex flex-col items-center gap-[8px]">
+				<h4 className="text-[16px] font-inter font-light text-white text-center">
+					{ t('orderWebsite') }
+				</h4>
+				<a
+					href={ `tel:${ t('orderWebsitePhone').replace(/\s/g, '') }` }
+					className="text-[12px] font-inter font-extralight text-white hover:opacity-80 transition-opacity"
+				>
+					{ t('orderWebsitePhone') }
+				</a>
+			</div>
 		</footer>
-	)
+	);
 }
