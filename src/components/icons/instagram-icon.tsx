82191845002fa54098b630d245a371e28a3a9474
vlikaa <aliveliyev123654@gmail.com>
2025-11-23 20:59:47 +0400

    feat: add new social media icon components

---
+++ src/components/icons/instagram-icon.tsx
@@ -0,0 +1,10 @@
+export function InstagramIcon() {
+	return (
+		<svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<circle cx="17" cy="17" r="17" fill="#CCF7E1"/>
+			<path d="M22 13.5C22.8284 13.5 23.5 12.8284 23.5 12C23.5 11.1716 22.8284 10.5 22 10.5C21.1716 10.5 20.5 11.1716 20.5 12C20.5 12.8284 21.1716 13.5 22 13.5Z" fill="#00823F"/>
+			<path d="M21 8C23.76 8 26 10.24 26 13V21C26 23.76 23.76 26 21 26H13C10.24 26 8 23.76 8 21V13C8 10.24 10.24 8 13 8H17H21Z" stroke="#00823F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+			<path d="M17 13C19.21 13 21 14.79 21 17C21 19.21 19.21 21 17 21C14.79 21 13 19.21 13 17C13 14.79 14.79 13 17 13Z" stroke="#00823F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+		</svg>
+	);
+}
