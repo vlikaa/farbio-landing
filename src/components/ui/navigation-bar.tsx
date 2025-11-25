a228ed0145f1ba8f866997c7f98cd9561c13042d
vlikaa <aliveliyev123654@gmail.com>
2025-11-25 23:08:14 +0400

    fix: update NavigationBar component to improve layout by adjusting visibility and spacing of items

--- src/components/ui/navigation-bar.tsx
+++ src/components/ui/navigation-bar.tsx
@@ -4,7 +4,7 @@ import { NavigationBarProps } from '@/types/navigation';
 export default function NavigationBar({ items }: NavigationBarProps) {
 	return (
 		<nav>
-			<ul className="hidden md:flex md:gap-[20px] xl:gap-[50px]">
+			<ul className="hidden xl:flex xl:gap-[50px]">
 				{ items.map((item) => (
 					<NavigationItem item={ item } key={ item.name }/>
 				)) }
