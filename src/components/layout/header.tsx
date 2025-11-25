567d607a7712915add6fcc3fb48dd678efe0c64a
vlikaa <aliveliyev123654@gmail.com>
2025-11-25 23:05:04 +0400

    feat: implement responsive header with burger menu and language switcher for improved mobile navigation

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,6 +6,9 @@ import NavigationBar from '@/components/ui/navigation-bar';
 import Button from '@/components/ui/button';
 import LanguageSwitcher from '@/components/ui/language-switcher';
 import { useTranslations } from 'use-intl';
+import { useState } from 'react';
+import BurgerButton from '@/components/ui/burger-button';
+import BurgerMenu from '@/components/ui/burger-menu';
 
 const navigationKeys = [
 	{ key: 'services', href: '#services' },
@@ -16,6 +19,16 @@ const navigationKeys = [
 
 export default function Header() {
 	const t = useTranslations('Header');
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+	const handleMenuToggle = () => {
+		setIsMenuOpen(!isMenuOpen)
+	}
+
+	const handleMenuClose = () => {
+		setIsMenuOpen(false)
+	}
+
 
 	const navigation = navigationKeys.map(item => ({
 		name: t(item.key),
@@ -25,7 +38,7 @@ export default function Header() {
 
 	return (
 		<header className="sticky z-20 top-0 left-0 bg-white">
-			<div className="grid grid-cols-3 items-center h-[50px] px-[20px] md:px-[60px]">
+			<div className="flex justify-between items-center h-[50px] px-[20px] xl:grid md:grid-cols-3 md:px-[60px] 2xl:px-[120px]">
 				<div className="flex justify-start">
 					<Link href="/">
 						<Logo/>
@@ -33,12 +46,19 @@ export default function Header() {
 				</div>
 
 				<div className="flex justify-center">
-					<NavigationBar items={navigation}/>
+					<NavigationBar items={ navigation }/>
 				</div>
 
-				<div className="flex justify-end gap-[20px]">
-					<LanguageSwitcher />
-					<Button className="flex items-center justify-center rounded-md h-[30px] w-[177px] bg-[#00823F] text-white cursor-pointer hover:opacity-90" title={t('call')}/>
+				<div className="hidden xl:flex xl:justify-end xl:gap-[20px]">
+					<LanguageSwitcher/>
+					<Button
+						className="flex items-center justify-center rounded-md h-[30px] w-[177px] bg-[#00823F] text-white cursor-pointer hover:opacity-90"
+						title={ t('call') }/>
+				</div>
+
+				<div className="justify-end xl:hidden">
+					<BurgerButton isOpen={ isMenuOpen } onClick={ handleMenuToggle }/>
+					<BurgerMenu isOpen={ isMenuOpen } onClose={ handleMenuClose } items={ navigation }/>
 				</div>
 			</div>
 		</header>
