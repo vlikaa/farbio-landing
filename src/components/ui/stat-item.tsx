30e70ca5ca677c4210f473ccce0f88baa7c45cd7
vlikaa <aliveliyev123654@gmail.com>
2025-11-25 22:54:33 +0400

    fix: update StatItem component styles for improved responsiveness and visual hierarchy

--- src/components/ui/stat-item.tsx
+++ src/components/ui/stat-item.tsx
@@ -8,10 +8,10 @@ export function StatItem({ label, value, className }: StatItemProps) {
 	return (
 		<div className={ `flex flex-col ${ className }` }>
 			<span
-				className="text-[16px] md:text-[24px] font-inter font-bold md:font-extrabold md:font-roboto text-[#00823F]">
+				className="text-[20px] font-inter font-bold text-[#00823F] md:text-[30px] md:font-extrabold md:font-roboto xl:text-[32px]">
 				{ label }
 			</span>
-			<span className="text-[12px] md:text-[16px] font-inter font-extralight text-[#2D2D2D]">
+			<span className="text-[12px] font-inter font-extralight text-[#2D2D2D] leading-[8px] md:text-[16px] md:leading-none">
 				{ value }
 			</span>
 		</div>
