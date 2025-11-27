c519fc9916d94f988a5f7f6f36c73900a88eb392
vlikaa <aliveliyev123654@gmail.com>
2025-11-27 21:55:57 +0400

    feat: enhance LanguageSwitcher component with Popover and Command for improved language selection UI

--- src/components/ui/language-switcher.tsx
+++ src/components/ui/language-switcher.tsx
@@ -1,5 +1,21 @@
 'use client'
 
+import * as React from 'react'
+import { CheckIcon, ChevronsUpDownIcon } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import {
+	Command,
+	CommandGroup,
+	CommandItem,
+	CommandList,
+} from '@/components/ui/command'
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover'
+import { Button } from '@/components/ui/button';
 import { useLocale } from 'next-intl';
 import { useRouter, usePathname } from '@/i18n/navigation';
 import { routing } from '@/i18n/routing';
@@ -9,31 +25,49 @@ export default function LanguageSwitcher() {
 	const router = useRouter();
 	const pathname = usePathname();
 
-	const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-		const newLocale = e.target.value;
-		router.replace(pathname, { locale: newLocale });
-	};
+	const [open, setOpen] = React.useState(false)
+	const [value, setValue] = React.useState(locale)
 
 	return (
-		<select
-			value={ locale }
-			onChange={ handleLanguageChange }
-			className="font-inter font-light text-[16px] cursor-pointer"
-		>
-			{ routing.locales.map((loc) => (
-				<option
-					key={ loc }
-					value={ loc }
-					className="cursor-pointer"
-					style={{ 
-						color: 'red', 
-						backgroundColor: '#333',
-						fontSize: '14px'
-					}}
+		<Popover open={ open } onOpenChange={ setOpen }>
+			<PopoverTrigger asChild>
+				<Button
+					variant="outline"
+					role="combobox"
+					aria-expanded={ open }
+					className="justify-between"
 				>
-					{ loc.toUpperCase() }
-				</option>
-			)) }
-		</select>
-	);
+					{ locale.toUpperCase() }
+					<ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50"/>
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-[90px] p-0">
+				<Command>
+					<CommandList>
+						<CommandGroup>
+							{ routing.locales.map((loc) => (
+								<CommandItem
+									key={ loc }
+									value={ loc }
+									onSelect={ (currentValue) => {
+										router.replace(pathname, { locale: loc });
+										setValue(currentValue === value ? '' : currentValue)
+										setOpen(false)
+									} }
+								>
+									<CheckIcon
+										className={ cn(
+											'mr-2 h-4 w-4',
+											value === loc ? 'opacity-100' : 'opacity-0'
+										) }
+									/>
+									{ loc.toUpperCase() }
+								</CommandItem>
+							)) }
+						</CommandGroup>
+					</CommandList>
+				</Command>
+			</PopoverContent>
+		</Popover>
+	)
 }
