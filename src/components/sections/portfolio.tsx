9c243300bf1c75e271393558796c6899f0fe3895
vlikaa <aliveliyev123654@gmail.com>
2025-12-08 23:16:02 +0400

    refactor: update Portfolio component to use async translations with next-intl and replace image rendering with a Gallery component

--- src/components/sections/portfolio.tsx
+++ src/components/sections/portfolio.tsx
@@ -1,14 +1,9 @@
-'use client'
-
-import { useState } from 'react';
-import { useTranslations } from 'use-intl';
+import { getTranslations } from 'next-intl/server';
 import SectionTitle from '@/components/ui/section-title';
-import { PortfolioImage } from '@/components/ui/portfolio-image';
+import Gallery from '@/components/ui/gallery';
 
-
-export default function Portfolio() {
-	const t = useTranslations('Portfolio');
-	const [showAll, setShowAll] = useState(false);
+export default async function Portfolio() {
+	const t = await getTranslations('Portfolio');
 
 	const images = [
 		'/images/portfolio/img.png',
@@ -19,50 +14,36 @@ export default function Portfolio() {
 		'/images/portfolio/img_5.png',
 		'/images/portfolio/img_6.png',
 		'/images/portfolio/img_7.png',
-		'/images/portfolio/img.png',
-		'/images/portfolio/img_1.png',
-		'/images/portfolio/img_2.png',
-		'/images/portfolio/img_3.png',
-		'/images/portfolio/img_4.png',
-		'/images/portfolio/img_5.png',
-		'/images/portfolio/img_6.png',
-		'/images/portfolio/img_7.png',
+		'/images/portfolio/img_8.png',
+		'/images/portfolio/img_9.png',
+		'/images/portfolio/img_10.png',
+		'/images/portfolio/img_11.png',
+		'/images/portfolio/img_12.png',
+		'/images/portfolio/img_13.png',
+		'/images/portfolio/img_14.png',
+		'/images/portfolio/img_15.png',
 	];
-
-	const initialImages = 8;
-	const displayedImages = showAll ? images : images.slice(0, initialImages);
 
 	return (
 		<div id="portfolio" className="relative w-full min-h-[877px] pb-[50px] pt-[100px] md:py-[100px] px-4 md:px-6 bg-white xl:py-[100px] 2xl:py-[200px]">
 			<div className="flex flex-col items-center">
-				<SectionTitle title={ t('tag') }/>
+				<SectionTitle title={t('tag')} />
 
 				<h2 className="mt-[20px] mb-[40px] text-[26px] font-inter font-bold text-center md:mt-[40px] md:text-[48px] md:font-roboto md:font-extrabold xl:mb-[60px] 2xl:mb-[100px]">
-					{ t.rich('title', {
-						green: (chunks) => <span className="text-[#00823F]">{ chunks }</span>
-					}) }
+					{t.rich('title', {
+						green: (chunks) => <span className="text-[#00823F]">{chunks}</span>
+					})}
 				</h2>
 
-				<div className="grid grid-cols-2 md:grid-cols-4 gap-x-[10px] gap-y-[20px] mb-[40px] md:gap-x-[20px] md:gap-y-[30px] xl:mb-[60px] 2xl:grid-cols-6 2xl:mb-[100px]">
-					{ displayedImages.map((src, index) => {
-						const imageNumber = index + 1;
-						const altText = `${ t('tag') } - (${ imageNumber })`;
-						return (
-							<PortfolioImage 
-								key={ `${ src }-${ index }` } 
-								src={ src } 
-								alt={ altText }
-							/>
-						);
-					}) }
-				</div>
-
-				<button
-					onClick={ () => setShowAll(!showAll) }
-					className="text-[18px] md:text-[20px] font-roboto font-extrabold text-[#00572A] underline hover:opacity-80 transition-opacity 2xl:text-[24px]"
-				>
-					{showAll ? t('viewLess') : t('viewMore')}
-				</button>
+				<Gallery
+					images={images}
+					initialImagesCount={8}
+					translations={{
+						tag: t('tag'),
+						viewMore: t('viewMore'),
+						viewLess: t('viewLess')
+					}}
+				/>
 			</div>
 		</div>
 	);
